7e4359ed6d6ba618c2ca1805e08cc87eda774e1a
MayMessaoudLamekh <may.messaoudlamekh@esprit.tn>
2025-11-08 15:50:18 +0100

    Initial Commit

---
+++ Esprit Ios/Esprit_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Esprit_IosApp.swift
+//  Esprit Ios
+//
+//  Created by Mac de Mimi on 8/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Esprit_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
